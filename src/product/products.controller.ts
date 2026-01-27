552caf406997b2445c533c369ea3c1767dac7f25
Aavash Karmacharya <karmacharya.aavash0@gmail.com>
2026-01-27 12:42:58 +0545

    added cart stuff

--- src/product/products.controller.ts
+++ src/product/products.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { product } from './products.entity';
 import { productservice } from './products.service';
 
@@ -8,5 +16,17 @@ export class productcontroller {
   @Post('/createproduct')
   async createproduct(@Body() dto: product) {
     return await this.productservice.createproduct(dto);
+  }
+  @Get('getproduct/:product')
+  async getproductbyname(@Param('product') product: string) {
+    return await this.productservice.getproductbyname(product);
+  }
+  @Delete('deleteproduct/:id')
+  async deleteproductbyid(@Param('id') id: number) {
+    return await this.productservice.deleteproduct(id);
+  }
+  @Patch('updateproduct/:id')
+  async updateproduct(@Param('id') id: number, @Body() dto: product) {
+    return await this.productservice.updatproduct(id, dto);
   }
 }
